f26dfd79cd0e7dee318adb12571c644593a68c18
Lionel Herrero Battista <lionherrero@gmail.com>
2023-06-14 11:27:24 -0300

    Finished lesson: lambda expressions.

---
+++ c_sharp_advanced/Book.cs
@@ -0,0 +1,12 @@
+﻿namespace c_sharp_advanced
+{
+    public class Book
+    {
+        public Book()
+        {
+        }
+
+        public string Title { get; set; }
+        public int Price { get; set; }
+    }
+}
--- c_sharp_advanced/Program.cs
+++ c_sharp_advanced/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +9,31 @@ namespace c_sharp_advanced
     {
         static void Main(string[] args)
         {
+            // delegate = args => expression 
+            // () => ... no arguments
+            // x => ... one argument
+            // (x, y, z) => ... multiple arguments
+            //Func<int, int> square = number => number * number;
+            //Console.WriteLine(square(5));
+
+            //scope
+            //const int factor = 5;
+
+            //Func<int, int> multiplier = n => n * factor;
+
+            //var result = multiplier(10);
+
+            //Console.WriteLine(result);
+
+            // real world example
+            var books = new BookRepository().GetBooks();
+
+            books.FindAll(book => book.Price < 10); 
+
+            foreach (var book in books)
+            {
+                Console.WriteLine(book.Title);
+            }
         }
     }
 }
